1e5752df6ca8ae876a44d556328cb7c05395a9b9
HongmookChoi <leochoi224@gmail.com>
2026-07-10 15:21:52 +0900

    refactor: 폴더명 오타 수정 (Ract-Native -> React-Native) 및 .gitignore 업데이트

--- Ract-Native/index.js
+++
@@ -1,6 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// registerRootComponent는 Expo Go / native build 양쪽 환경에서
-// AppRegistry.registerComponent를 자동으로 호출해줍니다.
-registerRootComponent(App);
